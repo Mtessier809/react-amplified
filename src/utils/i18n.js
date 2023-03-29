24887d341e6a4f2525dbb5488f605e787a4256d3
Mathis Tessier <mathis.tessier@intellimind.com>
2023-03-29 11:20:05 -0700

    got rid of language detector

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,10 +1,10 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanuguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 
-i18next.use(initReactI18next).use(LanuguageDetector).use(Backend).init({
+i18next.use(initReactI18next).use(Backend).init({
   load: "languageOnly",
   debug: true,
+  lng: "en",
   fallbackLng: "en",
 });
