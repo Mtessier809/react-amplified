cdfa65bbc3301b014d8de92093bc61d1dbdec8b7
Mathis Tessier <mathis.tessier@intellimind.com>
2023-03-29 10:46:43 -0700

    changed i18next settings

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -4,6 +4,7 @@ import LanuguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 
 i18next.use(initReactI18next).use(LanuguageDetector).use(Backend).init({
+  load: "languageOnly",
   debug: true,
   fallbackLng: "en",
 });
