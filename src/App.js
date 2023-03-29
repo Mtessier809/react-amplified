f64f3ca434159c81f2e3e114b949e7cf5607ffc8
Mathis Tessier <mathis.tessier@intellimind.com>
2023-03-29 10:37:39 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useTranslation, Trans } from "react-i18next";
+function App() {
+  const { t } = useTranslation();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <Trans i18nKey="description">
+            Edit <code>src/App.js</code> and save to reload.
+          </Trans>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org  "
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t("learn")}
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
